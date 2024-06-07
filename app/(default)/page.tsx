3852c26664f2d10c2cfe7edc145ab476c1849c8d
Kevin Kamto <kamtokevin@gmail.com>
2024-06-06 22:17:48 -0400

    refactor: renamed metadata to appropriate labels

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: 'Home - Open PRO',
-  description: 'Page description',
+  title: 'Prediction Labs',
+  description: 'Corporate landing page for the Prediction Labs company',
 }
 
 import Hero from '@/components/hero'
